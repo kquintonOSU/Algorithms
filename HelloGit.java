e6294870fad1d9505089d64295e3791af219bb2d
Kolton Quinton <kolton.quinton@gmail.com>
2023-03-15 16:44:53 -0500

    edit

--- HelloGit.java
+++ HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
-        System.out.println("Successfully set up Repo")
+        System.out.println("Successfully set up Repo");
     }
 }
