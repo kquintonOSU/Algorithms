6b328ca015d01862caced81f318ac33d03df56b8
Kolton Quinton <kolton.quinton@gmail.com>
2023-03-21 20:09:12 -0500

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,33 @@
+public class InsertionSort {
+
+    public void sort(int[] arr) {
+        for(int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j -= 1;
+            }
+
+            arr[j + 1] = temp;
+        }
+    }
+
+    public void printArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {5, 1, 2, 9, 10};
+
+        InsertionSort is = new InsertionSort();
+        is.printArray(arr);
+        is.sort(arr);
+        is.printArray(arr);
+    }
+}
